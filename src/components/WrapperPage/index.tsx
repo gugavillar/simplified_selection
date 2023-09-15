be113576b76a23f14721b5a17144cb2fc7467c5f
Guga Villar <grsv.21@gmail.com>
2023-09-15 07:42:28 -0300

    :bug: Required title

--- src/components/WrapperPage/index.tsx
+++ src/components/WrapperPage/index.tsx
@@ -3,17 +3,19 @@ import { PropsWithChildren, Suspense } from 'react'
 import { LoaderContent } from '@/components/LoaderContent'
 
 type WrapperPageProps = PropsWithChildren & {
-  title: string
+  title?: string
 }
 
 export const WrapperPage = ({ children, title }: WrapperPageProps) => {
   return (
     <div className="mx-auto max-w-5xl pb-6">
-      <div className="mx-auto max-w-lg text-center">
-        <h1 className="text-matisse-600 text-xl font-bold sm:text-3xl mb-6">
-          {title}
-        </h1>
-      </div>
+      {title ? (
+        <div className="mx-auto max-w-lg text-center">
+          <h1 className="text-matisse-600 text-xl font-bold sm:text-3xl mb-6">
+            {title}
+          </h1>
+        </div>
+      ) : null}
       <Suspense fallback={<LoaderContent />}>{children}</Suspense>
     </div>
   )
